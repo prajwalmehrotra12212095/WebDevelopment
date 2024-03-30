e58b81f85089cfca687d7a46ed7a464136d3fda0
prajwalmehrotra12212095 <prajwalmehrotra234@gmail.com>
2024-03-30 20:43:44 +0530

    message

--- Javascript/dom-api-examples.js
+++ Javascript/dom-api-examples.js
@@ -1,16 +1,22 @@
 const para = document.createElement("p")
 para.textContent = "thisis by using dom api";
+para.classList.add("one")
 
-
-para.setAttribute("id","js.para");
-console.log(para.getAtttribute("id"))
-const span = document.createElement("span")//dom node
+const span = document.createElement("span") //dom node
+// para.setAttribute("id","js.para");
+// console.log(para.getAtttribute("id"))
 
 
 span.id= "span-id";
-span.className = "blue";
-span.textContent="this is using span"
-span.style.backgroundColor="yellow"
-para.appendChild(span);
-
+ span.className = "blue";
+ span.textContent="this is using span <em>fggfgf</em>"
+// span.style.backgroundColor="yellow"
+ span.innerHTML="<em>hey its to change the font</em>"
+ para.appendChild(span);
+const hobbies = ["coding","swimming","skating"]
+const list = document.createElement("ul");
+for(let hobby of hobbies){
+    list.innerHTML += "<li class = 'blue'>" + hobby + "</li>"
+    document.body.appendChild(list);
+}
 document.body.appendChild(para);
