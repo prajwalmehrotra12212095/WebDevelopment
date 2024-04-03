94cb9568c5e5c8cf2f3e494031cd47bdb587c709
prajwalmehrotra12212095 <prajwalmehrotra234@gmail.com>
2024-04-03 20:41:51 +0530

    message

--- event handling/forms.js
+++ event handling/forms.js
@@ -1,6 +1,16 @@
 document.addEventListener("DOMContentLoaded" ,function(){
+console.log(document.getElementById("outside").value);
    document.querySelector("form").addEventListener("submit",function(event){
     event.preventDefault();
-    console.log(event);
+    const form = event.target
+    console.log(form.username.value)
+    console.log(form.mobile.value)
+    const mobileNumber = form.mobile.value;
+    console.log("this mobile number is "+mobileNumber)
+    console.log(form.email.value);
    }) 
+   document.addEventListener("click",function(){
+
+   })
+   console.log(document.getElementById("outside").value);
 })
