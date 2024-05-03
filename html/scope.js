1911218afdc943cd0d06beb8a7246a558af72501
prajwalmehrotra12212095 <prajwalmehrotra234@gmail.com>
2024-05-03 20:33:00 +0530

    message

--- html/scope.js
+++ html/scope.js
@@ -15,15 +15,32 @@
 // printMe();
 // conssole.log(a)
 //hoisting
-console.log(somevariable);
-someFunction();
-function someFunction(){
-    console.log("from someFunction", somevariable);
-}
-var somevariable = "devsnest"; //declaration
+// console.log(somevariable);
+// someFunction();
+// function someFunction(){
+//     console.log("from someFunction", somevariable);
+// }
+// var somevariable = "devsnest"; //declaration
+// console.log(somevariable);
+// someFunction();
 // execution content is created
 
 // creation phase
 // loads variable and func in memory
 // execute statemnet line by line
-
+var myName = "gaurav"; 
+function printName(){
+    console.log(myName);
+    printAnotherName();
+}
+function printAnotherName(){
+    var myName = "john";
+    console.log(myName);
+    printSomeOtherName();
+}
+function printSomeOtherName(){
+    // var myName = "jane";
+    console.log(myName);
+}
+console.log(myName);
+printName();
