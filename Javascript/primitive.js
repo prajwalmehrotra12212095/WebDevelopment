fcac7bf669a2ac705a82fd5bc55ebd2d571f88d6
prajwalmehrotra12212095 <prajwalmehrotra234@gmail.com>
2024-10-11 02:20:59 +0530

    message

---
+++ Javascript/primitive.js
@@ -0,0 +1,17 @@
+let a = null;
+let b = 345;
+let c = true;
+let d= BigInt("567")
+let e = "harry"
+let f = Symbol("i am a nice")
+let g = undefined
+console.log(a,b,c,d,e,f,g)
+console.log(typeof d) 
+//objects
+const item = {
+    "harry": true,
+    "Shubh": false,
+    "Lovaish":67,
+    "rohan": undefined
+}
+console.log(item["harry"])
