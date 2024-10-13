782f65518ffb08d43e11edc63fb5e107386ff3c3
prajwalmehrotra12212095 <prajwalmehrotra234@gmail.com>
2024-10-13 13:24:26 +0530

    messg

--- Javascript/primitive.js
+++ Javascript/primitive.js
@@ -1,17 +1,28 @@
-let a = null;
-let b = 345;
-let c = true;
-let d= BigInt("567")
-let e = "harry"
-let f = Symbol("i am a nice")
-let g = undefined
-console.log(a,b,c,d,e,f,g)
-console.log(typeof d) 
-//objects
-const item = {
-    "harry": true,
-    "Shubh": false,
-    "Lovaish":67,
-    "rohan": undefined
+// let a = null;
+// let b = 345;
+// let c = true;
+// let d= BigInt("567")
+// let e = "harry"
+// let f = Symbol("i am a nice")
+// let g = undefined
+// console.log(a,b,c,d,e,f,g)
+// console.log(typeof d) 
+// //objects
+// const item = {
+//     "harry": true,
+//     "Shubh": false,
+//     "Lovaish":67,
+//     "rohan": undefined
+// }
+// console.log(item["harry"])
+let a = "hrry"
+let b = 3
+console.log(a+b)
+console.log(typeof(a+b))
+const a1={
+    name: "jagg",
+    section: 1,
+    isPrincipal: false
 }
-console.log(item["harry"])
+a1['friend'] = "shubham"
+console.log(a1)
