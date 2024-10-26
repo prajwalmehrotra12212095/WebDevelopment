9da9a68abed529e19a6db5864c1c100a254d7043
prajwalmehrotra12212095 <prajwalmehrotra234@gmail.com>
2024-10-26 21:52:17 +0530

    message

--- Javascript/codnition.js
+++ Javascript/codnition.js
@@ -2,8 +2,17 @@
 // let a = prompt("Hey, what's your age? ");
 // a = Number.parseInt(a); 
 // console.log(typeof a); 
-const expr = 'parer';
-switch(expr){
-    case 'oranges':
-        console.log("hcfhssfd");
-}
+// const expr = 'parer';
+// switch(expr){
+//     case 'oranges':
+//         console.log("hcfhssfd");
+// }
+
+alert("using javascript");
+prompt()
+console.log("Hi projwal");
+
+let a = "prajwal";
+ //string
+console.log(typeof a)
+console.log(a);
