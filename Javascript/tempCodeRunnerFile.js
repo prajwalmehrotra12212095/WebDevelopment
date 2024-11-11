5918adf35b3a814a8b7473277e35dbf16888e485
prajwalmehrotra12212095 <prajwalmehrotra234@gmail.com>
2024-11-11 20:51:39 +0530

    mesage

---
+++ Javascript/tempCodeRunnerFile.js
@@ -0,0 +1,7 @@
+const n = ("enter the value of n")
+n = Number.parseInt(n)
+let i =0;
+while(i<n){
+    console.log(i)
+    i++;
+}
---
+++ Javascript/while.js
@@ -0,0 +1,11 @@
+let n = prompt("enter the value of n")
+n = Number.parseInt(n)
+// let i =0;
+// while(i<n){
+//     console.log(i)
+//     i++;
+let i =0;
+do{
+    console.log(i)
+    i++;
+}while(i<n)
