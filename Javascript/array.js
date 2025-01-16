7aa6df83de8a47e770fcf02ff83b9f35d04e6c74
prajwalmehrotra12212095 <prajwalmehrotra234@gmail.com>
2025-01-17 01:58:53 +0530

    learn

---
+++ Javascript/array.js
@@ -0,0 +1,16 @@
+let marks_class_12 = [91,56,89,73]
+console.log(marks_class_12)
+console.log(marks_class_12[0])
+console.log(marks_class_12.length)
+marks_class_12[4] = 89
+marks_class_12[0] = 99
+console.log(marks_class_12)
+let num = [1,2,3,4]
+let b =num.toString()
+console.log(b)
+let c =num.join("_")
+console.log(c)
+let d =num.pop()
+console.log(d,num)
+let e =num.push(56)
+console.log(num)
