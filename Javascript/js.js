ce50403193e665cb1c600b8f2fa62117dea50c66
prajwalmehrotra12212095 <prajwalmehrotra234@gmail.com>
2025-01-20 18:45:23 +0530

    mesage

--- Javascript/js.js
+++ Javascript/js.js
@@ -2,3 +2,15 @@ console.log("hello")
 let a = 9;
 let b = 10;
 console.log(a+b)
+alert("yur script work a!")
+let c = prompt("enter value")
+a = Number.parseInt(a)
+alert("you enter a type" + (typeof a))
+document.write(a)
+let write =confirm("f u want to wirte")
+if(write){
+    document.write(a)
+}
+else{
+    document.write("please allow")
+} 
