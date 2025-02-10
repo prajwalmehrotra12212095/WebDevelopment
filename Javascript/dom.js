d158a86d3ca95bb033198c0c60ff3923f91319bf
prajwalmehrotra12212095 <prajwalmehrotra234@gmail.com>
2025-02-11 01:27:44 +0530

    mesaage

---
+++ Javascript/dom.js
@@ -0,0 +1,13 @@
+// let button = document.getElementById("hi");
+// console.dir(button);
+// let heading = document.getElementsByClassName("first");
+// console.dir(heading);;
+// console.log(heading);
+
+let ele = document.querySelector("p");
+console.log(ele);
+
+let all = document.querySelectorAll("p");
+console.log(all);
+let eli = document.querySelector(".hi");
+console.log(eli);
