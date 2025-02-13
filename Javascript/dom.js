57d5f1229fbbbc394384b7660dbaf5f60267b733
prajwalmehrotra12212095 <prajwalmehrotra234@gmail.com>
2025-02-13 14:38:06 +0530

    mesgae

--- Javascript/dom.js
+++ Javascript/dom.js
@@ -11,3 +11,7 @@ let all = document.querySelectorAll("p");
 console.log(all);
 let eli = document.querySelector(".hi");
 console.log(eli);
+let a = document.querySelector("h3");
+console.log(a.innerText);
+a.innerText = a.innerText + "hey it me"
+
