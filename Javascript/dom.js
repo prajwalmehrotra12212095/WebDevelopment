79eda37308f64f84f6a9cad53781998eecca924d
prajwalmehrotra12212095 <prajwalmehrotra234@gmail.com>
2025-02-15 00:00:07 +0530

    mesage

--- Javascript/dom.js
+++ Javascript/dom.js
@@ -4,14 +4,42 @@
 // console.dir(heading);;
 // console.log(heading);
 
-let ele = document.querySelector("p");
-console.log(ele);
+// let ele = document.querySelector("p");
+// console.log(ele);
 
-let all = document.querySelectorAll("p");
-console.log(all);
-let eli = document.querySelector(".hi");
-console.log(eli);
-let a = document.querySelector("h3");
-console.log(a.innerText);
-a.innerText = a.innerText + "hey it me"
+// let all = document.querySelectorAll("p");
+// console.log(all);
+// let eli = document.querySelector(".hi");
+// console.log(eli);
+// let a = document.querySelector("h3");
+// console.log(a.innerText);
+// a.innerText = a.innerText + "hey it me"
+
+// let div = document.querySelector("div");
+// console.log(div);
+// let id = div.getAttribute("id");
+// console.log(id);
+
+// //get 
+// let h = document.querySelector("h1");
+// console.log(h);
+// let clas = h1.getAttribute("class");
+// console.log(clas);
+
+// //set
+// let hh = document.querySelector("p"); 
+// console.log(hh.setAttribute("class","name")); 
+// //styling
+// div.style.backgroundColor = "green";
+// div.innerText = "hellu";
+
+//create element
+
+let bo = document.createElement
+("button");
+bo.innerText = "click me";
+console.log(bo);
+
+let div = document.querySelector("div");
+div.append(bo)
 
