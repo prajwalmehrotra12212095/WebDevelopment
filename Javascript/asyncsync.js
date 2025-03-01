8a69bb704b9848f2f493632227cb627fd125e2b2
prajwalmehrotra12212095 <prajwalmehrotra234@gmail.com>
2025-03-02 04:04:42 +0530

    mesage

---
+++ Javascript/asyncsync.js
@@ -0,0 +1,15 @@
+//sync
+
+// function calculator(a,b,sumCallback){
+//     sumCallback(a,b);
+// }
+// calculator(1,2,(a,b) => {
+//     console.log(a+b);
+// });
+
+//async
+
+const hello = () => {
+    console.log("hy");
+}
+setTimeout(hello,3000);
