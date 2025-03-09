8fd5d52a4bf5d082f02416c5a9d1b6f00bddd82a
prajwalmehrotra12212095 <prajwalmehrotra234@gmail.com>
2025-03-10 01:23:58 +0530

    message

---
+++ Javascript/asyncawait.js
@@ -0,0 +1,41 @@
+async function hello() {
+    console.log("hello");
+}
+
+function api(){
+    return new Promise((resolve,reject) => {
+        setTimeout(() => {
+            console.log("weathercdata");
+            resolve(200);
+        },2000)
+    });
+}
+
+async function getWeather() {
+    await api();
+}
+
+function getData(dataId){
+    return new Promise((resolve,reject) => {
+        setTimeout (() => {
+            console.log("data",dataId);
+            resolve("success");
+},5000);
+    });
+}
+
+//async-await
+
+async function getAllData() {
+    await getData(1);
+    await getData(2);
+}
+
+//iife
+
+(async function (){
+    console.log("geting data 1");
+    await getData(1);
+    console.log("get data 2");
+    await getData(2);
+})();
