6c608e81364beab805a087959b7c7ac41fa54768
prajwalmehrotra12212095 <prajwalmehrotra234@gmail.com>
2025-04-03 01:55:49 +0530

    mesafe

--- Javascript/fetchapi.js
+++ Javascript/fetchapi.js
@@ -3,16 +3,24 @@ const factPara = document.querySelector("#fact");
 const btn = document.querySelector("#btn");
 
 
-// const getFacts = async () => {
-//     console.log("ghetting data....");
-//     let response = await fetch(URL);
-//     console.log(response); //json format
-//     let data = await response.json();
-//     console.log(data);
-//     //factPara.innerText = data[0].text;
-// };
+const getFacts = async () => {
+    console.log("ghetting data....");
+    let response = await fetch(URL);
+    console.log(response); //json format
+    let data = await response.json();
+    console.log(data);
+    //factPara.innerText = data[0].text;
+};
 
-
+// function getFacts() {
+//     fetch(URL)
+//     .then((response) => {
+//         return response.json();
+//     })
+//     .then ((data) => {
+//         console.log(data);
+//     });
+// }
 
 btn,addEventListener("click",getFacts);
  
